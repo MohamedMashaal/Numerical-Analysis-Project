f16e869aae09550ae3ce035cfb7af94af16f16f9
ahm7 <elpopahmed462@gmail.com>
2018-04-28 01:55:39 +0300

    bisection & falseposition

---
+++ falsePosition.m
@@ -0,0 +1,27 @@
+function root = falsePosition(Xu,Xl,es,Im);
+%this function get roots of equation by bisection method 
+str = '@(x)x^4+3*x-4'; % it will be input from the gui 
+f = str2func(str);
+format long
+if((f(Xl))*(f(Xu))>0) 
+   disp('no bracket');
+return
+end
+xrold = 0;
+for i=1:1:Im
+Xr= (Xl*f(Xu) - Xu*f(Xl)) / (f(Xu)-f(Xl)); % compute the midpoint xr
+ea = abs((Xr-xrold)/Xr); % approx. relative error
+test= f(Xl) * f(Xr); % compute f(xl)*f(xr)
+xrold = Xr ;
+s=sprintf('\n  xl = %f  xu = %f Root= %f #Iterations = %d ea = %f  f(Xr)= %f  \n', Xl,Xu,Xr,i,ea,f(Xr)); disp(s);
+if(test < 0) Xu=Xr;
+else Xl=Xr;
+end
+
+if(test == 0) ea=0; end
+if(ea < es) break; end
+end
+
+end   % end of quadratic
+
+
