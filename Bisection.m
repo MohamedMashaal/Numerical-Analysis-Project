2f55f54628f7faac44ad4e131b129be33ed7465d
ahm7 <elpopahmed462@gmail.com>
2018-05-04 11:38:46 +0300

    change return type of (bisection&false)to matrix

--- Bisection.m
+++ Bisection.m
@@ -1,4 +1,4 @@
-function root = Bisection(Xu,Xl,es,Im);
+function [result] = Bisection(Xu,Xl,es,Im);
 %this function get roots of equation by bisection method 
 str = '@(x)x.^3 - (0.165 .* x.^2)  + 3.993 .* 10.^(-4)'; % it will be input from the gui 
 f = str2func(str);
@@ -8,20 +8,24 @@ if((f(Xl))*(f(Xu))>0)
 return
 end
 xrold = 0;
+result= [0 0 0 0 0 0];
 for i=1:1:Im
 Xr=(Xu+Xl)/2; % compute the midpoint xr
 ea = abs((Xr-xrold)/Xr); % approx. relative error
 test= f(Xl) * f(Xr); % compute f(xl)*f(xr)
 xrold = Xr ;
 s=sprintf('\n  xl = %f  xu = %f Root= %f #Iterations = %d ea = %f  f(Xr)= %f  \n', Xl,Xu,Xr,i,ea,f(Xr)); disp(s);
+current= [i Xl Xu  Xr f(Xr) ea];
+result = [result;current];
 if(test < 0) Xu=Xr;
 else Xl=Xr;
 end
 
 if(test == 0) ea=0; end
 if(ea < es) break; end
-end
 
+end
+result([1],:) = [];
 end   % end of quadratic
 
 
--- falsePosition.m
+++ falsePosition.m
@@ -1,8 +1,9 @@
-function root = falsePosition(Xu,Xl,es,Im);
+function [result] = falsePosition(Xu,Xl,es,Im);
 %this function get roots of equation by bisection method 
 str = '@(x)x^4+3*x-4'; % it will be input from the gui 
 f = str2func(str);
 format long
+result= [0 0 0 0 0 0];
 if((f(Xl))*(f(Xu))>0) 
    disp('no bracket');
 return
@@ -14,6 +15,8 @@ ea = abs((Xr-xrold)/Xr); % approx. relative error
 test= f(Xl) * f(Xr); % compute f(xl)*f(xr)
 xrold = Xr ;
 s=sprintf('\n  xl = %f  xu = %f Root= %f #Iterations = %d ea = %f  f(Xr)= %f  \n', Xl,Xu,Xr,i,ea,f(Xr)); disp(s);
+current= [i Xl Xu  Xr f(Xr) ea];
+result = [result;current];
 if(test < 0) Xu=Xr;
 else Xl=Xr;
 end
@@ -21,7 +24,7 @@ end
 if(test == 0) ea=0; end
 if(ea < es) break; end
 end
-
+result([1],:) = [];
 end   % end of quadratic
 
 
